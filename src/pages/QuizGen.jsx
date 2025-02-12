6d06102ed9673ab647e9674bee83755f6f5b7fe5
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2025-02-12 16:52:32 -0500

    Refactor:
    Add className main-container and imported TopicSelect css

--- src/pages/QuizGen.jsx
+++ src/pages/QuizGen.jsx
@@ -1,8 +1,9 @@
 import TopicSelect from '../components/TopicSelect';
+import "../styles/TopicSelect.css";
 
 function QuizGen() {
     return (
-        <div>
+        <div className='main-container'>
             <h4>Quiz Generation Options</h4>
             <p className="grey-text">
                 Please choose your preferences below to generate your personalized quiz
