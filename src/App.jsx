b50a009aad7650cb3203a8d6c689363e3f1cdf3e
Angel D. <ajdupe08@gmail.com>
2025-02-12 17:21:47 -0500

    feat: add routing to app.jsx (#7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './pages/Home'
 import QuizGen from './pages/QuizGen';
+import QuizPage from './pages/QuizPage';
+import Account from './pages/Account';
+import Results from './pages/Results';
 
 function App() {
   return (
-    <div>
-      <QuizGen />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/quiz-gen" element={<QuizGen />} />
+        <Route path="/quiz-page" element={<QuizPage />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
+
 export default App;
